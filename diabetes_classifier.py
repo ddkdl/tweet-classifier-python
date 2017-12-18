27eda3e884b6c7e601e381545810ed496d13b671
Alvaro Esperanca <alvaro.sdb.esperanca@gmail.com>
2017-12-18 18:54:10 -0500

    removed old classification folders. Updated asthma and diabetes classifier

--- diabetes_classifier.py
+++ diabetes_classifier.py
@@ -5,24 +5,41 @@ from PreProcessor import PreProcessor
 from GaussianKernel import GaussianKernel
 
 def main():
-    dirList = list()
-
-    temp = listdir("./data/Diabetes/2010/")
-    dirList.append(temp)
-    temp = listdir("./data/Diabetes/2011/")
-    dirList.append(temp)
-    temp = listdir("./data/Diabetes/2012/")
-    dirList.append(temp)
-    temp = listdir("./data/Diabetes/2013/")
-    dirList.append(temp)
-    temp = listdir("./data/Diabetes/2014/")
-    dirList.append(temp)
-
+    
     pre = PreProcessor()
-    clf = SVM(kernel=GaussianKernel(5.0), C=1.0)
+    clf = SVM(kernel=GaussianKernel(5.0), C=1)
 
-    X_train, y_train = pre.loadTrainingSet("training_data/Diabetes_Sample_Tokenized.csv")    
+    # Loading training data
+    X_train, y_train = pre.loadTrainingSet("./training_data/Diabetes_Sample_Tokenized.csv")
+
+    # Training classifier
     clf.fit(X_train, y_train)
+
+    # Loading raw tweets
+    years = {"2010" : list(), "2011" : list(), "2012" : list(), "2013" : list(), "2014" : list()}
+
+    years["2010"] = listdir("./data/Diabetes/2010/")
+    years["2011"] = listdir("./data/Diabetes/2011/")
+    years["2012"] = listdir("./data/Diabetes/2012/")
+    years["2013"] = listdir("./data/Diabetes/2013/")
+    years["2014"] = listdir("./data/Diabetes/2014/")
+
+    # Classifying tweets
+    for year in ["2010", "2011", "2012", "2013", "2014"]:
+        for entry in years[year]:
+            print "Classifying %s %s" % (year, entry)
+            
+            X = pre.loadTestSet("./data/Diabetes/" + year + "/" + entry)
+            predictions = clf.predict(X)
+            
+            outputFile = entry.split(".")[0] + ".txt"
+            
+            predFile = open("./filtered_labels/Diabetes/" + year + "/" + outputFile, "w")
+
+            for prediction in predictions:
+                predFile.write("%d\n" % prediction)
+
+            predFile.close()
 
 if __name__ == '__main__':
     main()
